020a11d68d651fce4bb34db285972535b74f65d7
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-11 23:10:58 +0100

    manage user

--- lit_revu/reviews/admin.py
+++ lit_revu/reviews/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Ticket, Review, UserFollows
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    ]
+    list_filter = ["is_staff", "is_active"]
+    search_fields = ["username", "email"]
+    ordering = ["username"]
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Review)
 admin.site.register(UserFollows)
 
