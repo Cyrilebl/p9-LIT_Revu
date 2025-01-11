df0759d4520e0eeddc81413f6c353eedf4b31594
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-11 23:12:11 +0100

    placeholders

--- lit_revu/authentication/forms.py
+++ lit_revu/authentication/forms.py
@@ -1,8 +1,38 @@
+from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"})
+    )
+
+    error_messages = {
+        "invalid_login": "Nom d'utilisateur ou mot de passe incorrect.",
+        "inactive": "Ce compte est inactif.",
+    }
 
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"})
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Prénom"})
+    )
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Nom"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "E-mail"}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirmer le mot de passe"})
+    )
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ("username", "email", "first_name", "last_name")
