2b9f74cd3f1701446e8ff8b4dcbb619d244445b2
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-12 23:28:54 +0100

    urls organization

--- lit_revu/lit_revu/urls.py
+++ lit_revu/lit_revu/urls.py
@@ -8,35 +8,45 @@ from django.contrib.auth.views import (
     PasswordChangeView,
     PasswordChangeDoneView,
 )
-from authentication.forms import CustomLoginForm
+from authentication.forms import LoginForm, NewPasswordForm
 
 urlpatterns = [
+    # Authentication app
     path("admin/", admin.site.urls),
     path(
         "",
         LoginView.as_view(
             template_name="authentication/login.html",
-            authentication_form=CustomLoginForm,
+            authentication_form=LoginForm,
             redirect_authenticated_user=True,
         ),
         name="login",
     ),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", authentication.views.signup_page, name="signup"),
     path(
-        "password_change/",
+        "signup/done",
+        authentication.views.signup_done,
+        name="signup_done",
+    ),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", authentication.views.profile_page, name="profile"),
+    path(
+        "profile/password_change/",
         PasswordChangeView.as_view(
-            template_name="authentication/password_change_form.html"
+            form_class=NewPasswordForm,
+            template_name="authentication/password_change_form.html",
+            success_url="profile/password_change/done",
         ),
         name="password_change",
     ),
     path(
-        "password_change/done/",
+        "profile/password_change/done/",
         PasswordChangeDoneView.as_view(
             template_name="authentication/password_change_done.html"
         ),
         name="password_change_done",
     ),
-    path("signup/", authentication.views.signup_page, name="signup"),
-    path("tickets/", reviews.views.ticket_list, name="ticket-list"),
-    path("tickets/<int:ticket_id>/", reviews.views.ticket_detail, name="ticket-detail"),
+    # Reviews app
+    path("tickets/", reviews.views.ticket_list, name="ticket_list"),
+    path("tickets/<int:ticket_id>/", reviews.views.ticket_detail, name="ticket_detail"),
 ]
