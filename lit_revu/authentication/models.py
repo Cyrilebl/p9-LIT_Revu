229c60793688331e89071d854708d7addcdf074b
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-30 15:32:41 +0100

    use default user

--- lit_revu/authentication/models.py
+++ lit_revu/authentication/models.py
@@ -1,14 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
-    CREATOR = "CREATOR"
-    SUBSCRIBER = "SUBSCRIBER"
-
-    ROLE_CHOICE = (
-        (CREATOR, "Créator"),
-        (SUBSCRIBER, "Abonné"),
-    )
-
-    role = models.CharField(max_length=30, choices=ROLE_CHOICE)
+    pass
